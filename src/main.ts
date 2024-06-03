6c7ca9f45a71c28e3c9132098a0fdb77004a0ba4
ManeeshWije <m.mwije1@gmail.com>
2024-06-02 22:07:33 -0400

    init

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import "./style.css";
+
+// [name you want to appear, link]
+const links: Map<string, string> = new Map([
+    ["syncthing", "http://localhost:8384/"],
+    ["railway", "https://railway.app/dashboard"],
+    ["reddit", "https://www.reddit.com"],
+    ["mail", "https://mail.proton.me"],
+    ["youtube", "https://www.youtube.com"],
+    ["x", "https://www.x.com"],
+    ["github", "https://www.github.com"],
+    ["linkedin", "https://www.linkedin.com/feed/"],
+    ["amazon", "https://www.amazon.ca/"],
+]);
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <h1>Welcome Maneesh.</h1>
+  <div class="cards">
+    <div class="card0"></div>
+    <div class="card1"></div>
+    <div class="card2"></div>
+  </div>
+`;
+
+let counter = 0;
+const cardCount = 3;
+const cardElements = [
+    document.querySelector(`.card0`),
+    document.querySelector(`.card1`),
+    document.querySelector(`.card2`),
+];
+
+links.forEach((link, name) => {
+    const cardIndex = counter % cardCount;
+    const card = cardElements[cardIndex];
+    let a = document.createElement("a");
+    a.innerText = name;
+    a.href = link;
+    card?.appendChild(a);
+    counter += 1;
+});
